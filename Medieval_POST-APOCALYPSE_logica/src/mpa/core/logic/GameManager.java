0413a4f08a47704c70134bf50fbe50c291137687
Elia Devito <eliadevito@yahoo.it>
2014-05-13 18:10:28 +0200

    test GameManager

--- Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/GameManager.java
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/GameManager.java
@@ -8,15 +8,16 @@ public class GameManager
 	private World world;
 	private List<Player> players;
 
-	public GameManager(String mapname, int numplayer, int position[])
+	public GameManager(String mapname, int numplayer, String PlayersName[], int position[])
 	{
 		// Instazio il WM e gli dico di caricare la mappa mapname
 		WorldManager WorldManager = new WorldManager(mapname);
 
-		for (int pos : position)
+		for (int i = 0; i < numplayer; i++)
 		{
-			Headquarter playerHeadquarter = WorldManager.addHeadquarter(pos);
-			players.add(new Player("Pippo", 0, 0, 100, Level.NEWBIE, playerHeadquarter, 0)); // TODO
+			Headquarter playerHeadquarter = WorldManager.addHeadquarter(i);
+			players.add(new Player(PlayersName[i], playerHeadquarter.getX(), playerHeadquarter
+					.getY(), 100, Level.NEWBIE, playerHeadquarter, 0)); // TODO
 		}
 
 	}
