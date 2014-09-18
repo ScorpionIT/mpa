e3a151bc01ea7332db68392ac06c2f71fc43766d
Suinos93 <Suinos@localhost.localdomain>
2014-09-18 12:46:13 +0200

    Added field manager

--- Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/resource/Field.java
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/resource/Field.java
@@ -1,22 +1,38 @@
 package mpa.core.logic.resource;
 
-enum FieldState {HARVEST,GROWTH, PLOWING, SEEDING}; //RACCOLTO, CRESCITA, ARATURA, SEMINA 
+enum FieldState { PLOWING, SEEDING, GROWTH, HARVEST }; //RACCOLTO, CRESCITA, ARATURA, SEMINA 
 
 public class Field extends AbstractResource 
 {
 	private FieldState currentFieldState;
 	private boolean free;
+	private int foodCollected;
+	private int maximumFoodCollected;
 	
-	public Field(int x, int y, int providing, FieldState fieldState)
+	public Field( int x, int y, int providing, FieldState fieldState, int maximumFoodCollected )
 	{
 		super(x, y, providing);
 		this.currentFieldState = fieldState;
 		this.free = true;
+		this.foodCollected = 0;
+		this.maximumFoodCollected = maximumFoodCollected;
+		
 	}
 
 	public FieldState getCurrentFieldState()
 	{
 		return currentFieldState;
+	}
+	
+	public void advanceStatus()
+	{
+		if( this.currentFieldState.ordinal() < FieldState.values().length )
+			this.currentFieldState= this.currentFieldState.values()[currentFieldState.ordinal() + 1];
+		else 
+		{
+			this.currentFieldState = FieldState.values()[0];
+			
+		}
 	}
 
 	public void setCurrentFieldState(FieldState currentFieldState)
--- Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/resource/FieldManager.java
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/resource/FieldManager.java
@@ -1,13 +1,47 @@
 package mpa.core.logic.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+ 
+
 
 public class FieldManager extends Thread
 {
-	private int succhia;
-	
-	public FieldManager()
-	{
-		// TODO Auto-generated constructor 
+	List<Field> fields;
+	int time;
 		
+	public FieldManager( ArrayList<Field> fields, int time)
+	{
+		this.fields = fields;
+		this.time = time;
 	}
+
+	@Override
+	public void run()
+	{
+		try
+        {
+            while (true)
+            {
+                try
+                {
+                    sleep( this.time );
+                }
+                catch (final InterruptedException e)
+                {
+
+                }
+               for( final Field field: this.fields)
+               {
+            	   field.advanceStatus();
+               }
+            }
+        }
+        catch (final Exception e)
+        {
+        }
+		super.run();
+	}
+	
+	
 }
