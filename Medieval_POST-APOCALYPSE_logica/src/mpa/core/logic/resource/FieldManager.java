d7d0669b56bf953fc15ed8728ca94132d5d76f85
Suinos <Suinos@localhost.localdomain>
2014-09-18 11:41:38 +0200

    paola

---
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/resource/FieldManager.java
@@ -0,0 +1,13 @@
+package mpa.core.logic.resource;
+
+
+public class FieldManager extends Thread
+{
+	private int succhia;
+	
+	public FieldManager()
+	{
+		// TODO Auto-generated constructor 
+		
+	}
+}
