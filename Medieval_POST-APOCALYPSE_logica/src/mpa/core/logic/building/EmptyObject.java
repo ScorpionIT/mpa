af45b5d62dee78b38d6ff9ce92ebb5797cba0ac1
Suinos <neongodtheymade@gmail.com>
2014-10-03 17:47:29 +0200

    Added EmptyObject.

---
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/building/EmptyObject.java
@@ -0,0 +1,11 @@
+package mpa.core.logic.building;
+
+public class EmptyObject extends AbstractBuilding
+{
+
+	public EmptyObject(int x, int y, int damages) throws Exception
+	{
+		super(x, y);
+	}
+
+}
