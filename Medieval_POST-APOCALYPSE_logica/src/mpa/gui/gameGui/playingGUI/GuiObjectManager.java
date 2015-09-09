b712db8a8167b5347d1a0575baedeaf31757f1ec
AruQ <arcuri.paola.93@gmail.com>
2015-09-09 19:48:18 +0200

    add model

--- Medieval_POST-APOCALYPSE_logica/src/mpa/gui/gameGui/playingGUI/GuiObjectManager.java
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/gui/gameGui/playingGUI/GuiObjectManager.java
@@ -26,18 +26,18 @@ public class GuiObjectManager
 	private HashMap<String, Spatial> towerCrushers = new HashMap<>();
 	private HashMap<String, Spatial> towers = new HashMap<>();
 	private HashMap<String, Spatial> hitPlayers = new HashMap<>();
-	private float scalingFactor = 2.2f;
+	private float scalingFactor = 70.2f;
 
 	private float worldDimension;
 
 	private GameGui gameGui;
-	private Vector3f upVector = new Vector3f( 0, 1, 0 );
+	private Vector3f upVector = new Vector3f(0, 1, 0);
 	private String playingPlayer;
 
-	private GuiObjectManager( GameGui gameGui, String playingPlayer, boolean multiplayer )
+	private GuiObjectManager(GameGui gameGui, String playingPlayer, boolean multiplayer)
 	{
 		this.gameGui = gameGui;
-		if( !multiplayer )
+		if (!multiplayer)
 		{
 
 			// clickActionListener = new GameGuiClickListener( listenerImplementation, gameGui );
@@ -47,9 +47,9 @@ public class GuiObjectManager
 		this.playingPlayer = playingPlayer;
 	}
 
-	static void init( GameGui gm, String playingPlayer )
+	static void init(GameGui gm, String playingPlayer)
 	{
-		_guiGuiObjectManager = new GuiObjectManager( gm, playingPlayer, false );
+		_guiGuiObjectManager = new GuiObjectManager(gm, playingPlayer, false);
 	}
 
 	public static GuiObjectManager getInstance()
@@ -62,35 +62,33 @@ public class GuiObjectManager
 		return worldDimension;
 	}
 
-	public void setWorldDimension( float worldDimension )
+	public void setWorldDimension(float worldDimension)
 	{
 		this.worldDimension = worldDimension;
-		gameGui.createWorld( worldDimension );
+		gameGui.createWorld(worldDimension);
 	}
 
-	public void addPlayer( String name, Vector2f hqPosition, Vector2f gatheringPlace,
-			Vector2f direction )
+	public void addPlayer(String name, Vector2f hqPosition, Vector2f gatheringPlace, Vector2f direction)
 	{
-		gameGui.getAssetManager().registerLocator( "./Assets/Models/SeanDevlin.zip",
-				ZipLocator.class );
-		Spatial playerModel = gameGui.getAssetManager().loadModel( "SeanDevlin.mesh.xml" );
-		playerModel.scale( scalingFactor, scalingFactor, scalingFactor );
-		playerModel.setLocalTranslation( new Vector3f( gatheringPlace.x, 5, gatheringPlace.y ) );
-		Quaternion quad = new Quaternion();
-		quad.lookAt( new Vector3f( gatheringPlace.x, 0, gatheringPlace.y ), upVector );
-		playerModel.setLocalRotation( quad );
-		players.put( name, playerModel );
-		gameGui.attachPlayer( playerModel );
+		gameGui.getAssetManager().registerLocator("./Assets/Models/Monster.zip", ZipLocator.class);
+		Spatial playerModel = gameGui.getAssetManager().loadModel("Monster.mesh.xml");
+		playerModel.scale(scalingFactor, scalingFactor, scalingFactor);
+		playerModel.setLocalTranslation(new Vector3f(gatheringPlace.x, 10, gatheringPlace.y));
+		// Quaternion quad = new Quaternion();
+		// quad.lookAt(new Vector3f(gatheringPlace.x, 0, -gatheringPlace.y), upVector);
+		// playerModel.setLocalRotation(quad);
+		players.put(name, playerModel);
+		gameGui.attachPlayer(playerModel);
 
-		gameGui.getAssetManager().registerLocator( "Assets/Models/tower.zip", ZipLocator.class );
-		Spatial hqModel = gameGui.getAssetManager().loadModel( "tower.mesh.xml" );
-		hqModel.setLocalTranslation( new Vector3f( hqPosition.x, 0, hqPosition.y ) );
-		hqModel.rotate( 90, 0, 0 );
-		hqModel.scale( 0.05f );
-		BoundingBox worldBound = ( BoundingBox ) hqModel.getWorldBound();
-		System.out.println( " X + " + worldBound.getXExtent() + " Y + " + worldBound.getZExtent() );
-		headQuarters.put( name, hqModel );
-		gameGui.attachStaticObject( hqModel );
+		gameGui.getAssetManager().registerLocator("Assets/Models/baker_house.zip", ZipLocator.class);
+		Spatial hqModel = gameGui.getAssetManager().loadModel("baker_house.mesh.xml");
+		hqModel.setLocalTranslation(new Vector3f(hqPosition.x, 0, hqPosition.y));
+		hqModel.rotate(0, 90, 0);
+		hqModel.scale(8f);
+		BoundingBox worldBound = (BoundingBox) hqModel.getWorldBound();
+		System.out.println(" X + " + worldBound.getXExtent() + " Y + " + worldBound.getZExtent());
+		headQuarters.put(name, hqModel);
+		gameGui.attachStaticObject(hqModel);
 
 	}
 
@@ -99,16 +97,16 @@ public class GuiObjectManager
 		return playingPlayer;
 	}
 
-	public void setHitPlayers( ArrayList<String> hitPl )
+	public void setHitPlayers(ArrayList<String> hitPl)
 	{
 		hitPlayers.clear();
-		for( String s : hitPl )
-			if( players.containsKey( s ) )
-				hitPlayers.put( s, players.get( s ) );
-			else if( minions.containsKey( s ) )
-				hitPlayers.put( s, minions.get( s ) );
-			else if( towerCrushers.containsKey( s ) )
-				hitPlayers.put( s, towerCrushers.get( s ) );
+		for (String s : hitPl)
+			if (players.containsKey(s))
+				hitPlayers.put(s, players.get(s));
+			else if (minions.containsKey(s))
+				hitPlayers.put(s, minions.get(s));
+			else if (towerCrushers.containsKey(s))
+				hitPlayers.put(s, towerCrushers.get(s));
 	}
 
 	public ArrayList<Spatial> getHitCharacters()
@@ -116,8 +114,8 @@ public class GuiObjectManager
 		try
 		{
 			ArrayList<Spatial> hit = new ArrayList<>();
-			for( String s : hitPlayers.keySet() )
-				hit.add( hitPlayers.get( s ) );
+			for (String s : hitPlayers.keySet())
+				hit.add(hitPlayers.get(s));
 			return hit;
 		} finally
 		{
@@ -125,42 +123,38 @@ public class GuiObjectManager
 		}
 	}
 
-	public void addResource( String type, int ID, Vector2f position )
+	public void addResource(String type, int ID, Vector2f position)
 	{
 		Spatial model = null;
-		switch( type )
+		switch (type)
 		{
 			case "WOOD":
-				gameGui.getAssetManager().registerLocator( "Assets/Models/Wood.zip",
-						ZipLocator.class );
-				model = gameGui.getAssetManager().loadModel( "Tree.mesh.xml" );
-				woods.put( ID, model );
+				gameGui.getAssetManager().registerLocator("Assets/Models/Wood.zip", ZipLocator.class);
+				model = gameGui.getAssetManager().loadModel("Tree.mesh.xml");
+				woods.put(ID, model);
 			case "FIELD":
-				gameGui.getAssetManager().registerLocator( "Assets/Models/CornField.zip",
-						ZipLocator.class );
-				model = gameGui.getAssetManager().loadModel( "CornField.mesh.xml" );
-				fields.put( ID, model );
+				gameGui.getAssetManager().registerLocator("Assets/Models/CornField.zip", ZipLocator.class);
+				model = gameGui.getAssetManager().loadModel("CornField.mesh.xml");
+				fields.put(ID, model);
 			case "CAVE":
-				gameGui.getAssetManager().registerLocator( "Assets/Models/stones.zip",
-						ZipLocator.class );
-				model = gameGui.getAssetManager().loadModel( "stones.mesh.xml" );
-				caves.put( ID, model );
+				gameGui.getAssetManager().registerLocator("Assets/Models/stones.zip", ZipLocator.class);
+				model = gameGui.getAssetManager().loadModel("stones.mesh.xml");
+				caves.put(ID, model);
 			case "MINE":
-				gameGui.getAssetManager().registerLocator( "Assets/Models/Wood.zip",
-						ZipLocator.class );
-				model = gameGui.getAssetManager().loadModel( "Tree.mesh.xml" );
-				mines.put( ID, model );
+				gameGui.getAssetManager().registerLocator("Assets/Models/Wood.zip", ZipLocator.class);
+				model = gameGui.getAssetManager().loadModel("Tree.mesh.xml");
+				mines.put(ID, model);
 				// TODO
 		}
 
-		model.setLocalTranslation( new Vector3f( position.x, 0, position.y ) );
+		model.setLocalTranslation(new Vector3f(position.x, 0, position.y));
 
-		model.rotate( 90, 0, 0 );
-		gameGui.attachStaticObject( model );
+		model.rotate(90, 0, 0);
+		gameGui.attachStaticObject(model);
 
 	}
 
-	public void addMinion( String ID )
+	public void addMinion(String ID)
 	{
 		// for( int i = 0; i < quantity; i++ )
 		{
@@ -171,63 +165,63 @@ public class GuiObjectManager
 		}
 	}
 
-	public void addTowerCrusher( String ID )
+	public void addTowerCrusher(String ID)
 	{
 
 	}
 
-	public void addTower( Vector2f position, String ID )
+	public void addTower(Vector2f position, String ID)
 	{
-		gameGui.getAssetManager().registerLocator( "Assets/Models/tower.zip", ZipLocator.class );
-		Spatial tower = gameGui.getAssetManager().loadModel( "tower.mesh.xml" );
-		tower.setLocalTranslation( position.x, 0, position.y );
-		towers.put( ID, tower );
+		gameGui.getAssetManager().registerLocator("Assets/Models/tower.zip", ZipLocator.class);
+		Spatial tower = gameGui.getAssetManager().loadModel("tower.mesh.xml");
+		tower.setLocalTranslation(position.x, 0, position.y);
+		towers.put(ID, tower);
 	}
 
-	public void updatePlayerPosition( String name, Vector2f position, Vector2f direction )
+	public void updatePlayerPosition(String name, Vector2f position, Vector2f direction)
 	{
-		Spatial player = players.get( name );
-		player.setLocalTranslation( position.x, 0, position.y );
+		// Spatial player = players.get(name);
+		// player.setLocalTranslation(position.x, 10, position.y);
+		// Quaternion quad = new Quaternion();
+		// quad.lookAt(new Vector3f(direction.x, 0, -direction.y), upVector);
+		// player.setLocalRotation(quad);
+	}
+
+	public void updateMinionPosition(String ID, Vector2f position, Vector2f direction)
+	{
+		Spatial minion = minions.get(ID);
+		minion.setLocalTranslation(position.x, 0, position.y);
 		Quaternion quad = new Quaternion();
-		quad.lookAt( new Vector3f( -direction.x, 0, -direction.y ), upVector );
-		player.setLocalRotation( quad );
+		quad.lookAt(new Vector3f(-direction.x, 0, -direction.y), upVector);
+		minion.setLocalRotation(quad);
 	}
 
-	public void updateMinionPosition( String ID, Vector2f position, Vector2f direction )
+	public void updateTowerCrusherPosition(String ID, Vector2f position, Vector2f direction)
 	{
-		Spatial minion = minions.get( ID );
-		minion.setLocalTranslation( position.x, 0, position.y );
+		Spatial crusher = towerCrushers.get(ID);
+		crusher.setLocalTranslation(position.x, 0, position.y);
 		Quaternion quad = new Quaternion();
-		quad.lookAt( new Vector3f( -direction.x, 0, -direction.y ), upVector );
-		minion.setLocalRotation( quad );
+		quad.lookAt(new Vector3f(-direction.x, 0, -direction.y), upVector);
+		crusher.setLocalRotation(quad);
 	}
 
-	public void updateTowerCrusherPosition( String ID, Vector2f position, Vector2f direction )
+	public void removeTower(String ID)
 	{
-		Spatial crusher = towerCrushers.get( ID );
-		crusher.setLocalTranslation( position.x, 0, position.y );
-		Quaternion quad = new Quaternion();
-		quad.lookAt( new Vector3f( -direction.x, 0, -direction.y ), upVector );
-		crusher.setLocalRotation( quad );
+		gameGui.detachStaticObject(towers.remove(ID));
 	}
 
-	public void removeTower( String ID )
-	{
-		gameGui.detachStaticObject( towers.remove( ID ) );
-	}
-
-	public void removePlayer( String name )
+	public void removePlayer(String name)
 	{
 		// lancia animazione
-		gameGui.detachPlayer( players.remove( name ) );
-		players.remove( name );
+		gameGui.detachPlayer(players.remove(name));
+		players.remove(name);
 	}
 
-	public void removeMinion( String ID )
+	public void removeMinion(String ID)
 	{
 		// lancia animazione
-		gameGui.detachPlayer( minions.get( ID ) );
-		minions.remove( ID );
+		gameGui.detachPlayer(minions.get(ID));
+		minions.remove(ID);
 	}
 
 	// TODO retrieve informations
