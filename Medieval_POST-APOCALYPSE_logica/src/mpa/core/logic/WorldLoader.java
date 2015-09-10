feb91a53a46a6f36f55cdd848d8ddc0bee3e3a7a
AruQ <arcuri.paola.93@gmail.com>
2015-09-10 17:15:40 +0200

    add headquarterID

--- Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/WorldLoader.java
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/WorldLoader.java
@@ -11,9 +11,9 @@ public class WorldLoader
 {
 	MapInfo mapInfo;
 
-	public void loadMapInfo( MapInfoCreator mapInfoCreator, String path )
+	public void loadMapInfo(MapInfoCreator mapInfoCreator, String path)
 	{
-		mapInfo = mapInfoCreator.createMapInfo( path );
+		mapInfo = mapInfoCreator.createMapInfo(path);
 	}
 
 	public MapInfo getMapInfo()
@@ -21,36 +21,36 @@ public class WorldLoader
 		return mapInfo;
 	}
 
-	public World loadWorld( WorldCreator worldCreator )
+	public World loadWorld(WorldCreator worldCreator)
 	{
-		World world = worldCreator.createWorld( mapInfo );
+		World world = worldCreator.createWorld(mapInfo);
 		return world;
 
 	}
 
-	public int makePlayers( String playerName, World world, Pair<Float, Float> selectedHQ )
+	public int makePlayers(String playerName, World world, Pair<Float, Float> selectedHQ)
 	{
+		int headquarterID = 0;
 		ArrayList<Pair<Float, Float>> headQuarters = mapInfo.getHeadQuarters();
 		int selectedHQIndex = 0;
-		for( int i = 0; i < headQuarters.size(); i++ )
+		for (int i = 0; i < headQuarters.size(); i++)
 		{
-			Headquarter headquarter = new Headquarter( headQuarters.get( i ).getFirst(),
-					headQuarters.get( i ).getSecond(), null );
+			Headquarter headquarter = new Headquarter(headQuarters.get(i).getFirst(), headQuarters.get(i).getSecond(), null);
 			Vector2f gatheringPlace = headquarter.getGatheringPlace();
 
-			Player player = new Player( playerName, gatheringPlace.x, gatheringPlace.y, 100,
-					Level.NEWBIE, headquarter );
-			headquarter.setOwner( player );
-			world.addObject( headquarter );
-			if( selectedHQ.equals( headQuarters.get( i ) ) )
+			Player player = new Player(playerName, gatheringPlace.x, gatheringPlace.y, 100, Level.NEWBIE, headquarter);
+			headquarter.setOwner(player);
+			headquarter.setID(Integer.toString(headquarterID));
+			world.addObject(headquarter);
+			if (selectedHQ.equals(headQuarters.get(i)))
 			{
-				GameManager.getInstance().addPlayer( player );
+				GameManager.getInstance().addPlayer(player);
 				selectedHQIndex = i;
 			}
 			else
 			{
-				player.setName( "Paola Maledetta " + i );
-				GameManager.getInstance().addAIPlayer( player );
+				player.setName("Paola Maledetta " + i);
+				GameManager.getInstance().addAIPlayer(player);
 			}
 		}
 
