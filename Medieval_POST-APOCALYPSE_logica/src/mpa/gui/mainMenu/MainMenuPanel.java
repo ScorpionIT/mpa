9d1f641a7a62298270df3e09c72ed1194f2bbaf5
AruQ <arcuri.paola.93@gmail.com>
2015-09-11 21:23:35 +0200

    added image to MainMenuPanel

--- Medieval_POST-APOCALYPSE_logica/src/mpa/gui/mainMenu/MainMenuPanel.java
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/gui/mainMenu/MainMenuPanel.java
@@ -6,9 +6,12 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import mpa.core.util.GameProperties;
 
 public class MainMenuPanel extends JPanel
 {
@@ -23,77 +26,109 @@ public class MainMenuPanel extends JPanel
 	private JLabel mapEditor;
 	private JLabel options;
 	private JLabel quit;
+	private String textImagePath = GameProperties.getInstance().getPath("TextImagePath");
 
-	public MainMenuPanel( int x, int y, int width, int height )
+	public MainMenuPanel(int x, int y, int width, int height)
 	{
-		setBounds( x, y, width, height );
-		setLayout( null );
+		setBounds(x, y, width, height);
+
+		int yIncrement = (this.getHeight() - this.getHeight() / 2) / 6;
+		int yComponent = this.getHeight() / 4;
+		setLayout(null);
 		try
 		{
-			backgroundImage = ImageIO.read( new File( "Assets/BackgroundImages/mainMenu.jpg" ) );
-		} catch( IOException e1 )
+			backgroundImage = ImageIO.read(new File("Assets/BackgroundImages/mainMenu.jpg"));
+		} catch (IOException e1)
 		{
 			e1.printStackTrace();
 		}
 
-		title = new JLabel( "CI SARÀ UN'IMMAGINE" );
-		title.setBounds( 25 * width / 100, 0, 50 * width / 100, 10 * height / 100 );
-		title.setVisible( true );
-		add( title );
+		title = new JLabel("CI SARÀ UN'IMMAGINE");
+		title.setBounds(25 * width / 100, 0, 50 * width / 100, 10 * height / 100);
+		title.setVisible(true);
+		add(title);
 
-		singlePlayer = new JLabel( "Single Player" );
-		singlePlayer.setBounds( 5, 15 * height / 100, 25 * width / 100, 10 * height / 100 );
-		singlePlayer.setVisible( true );
-		add( singlePlayer );
+		singlePlayer = loadJLabel("SinglePlayer.png");
+		singlePlayer.setBounds(5, yComponent, 25 * width / 100, yIncrement);
+		singlePlayer.setVisible(true);
+		add(singlePlayer);
 
-		multiPlayer = new JLabel( "Multi Player" );
-		multiPlayer.setBounds( 5, 30 * height / 100, 25 * width / 100, 10 * height / 100 );
-		multiPlayer.setVisible( true );
-		add( multiPlayer );
+		yComponent += yIncrement;
 
-		createServer = new JLabel( "Create Server" );
-		createServer.setBounds( 5, 45 * height / 100, 25 * width / 100, 10 * height / 100 );
-		createServer.setVisible( true );
-		add( createServer );
+		multiPlayer = loadJLabel("Multiplayer.png");
+		multiPlayer.setBounds(5, yComponent, 25 * width / 100, yIncrement);
+		multiPlayer.setVisible(true);
+		add(multiPlayer);
 
-		mapEditor = new JLabel( "Map Editor" );
-		mapEditor.setBounds( 5, 60 * height / 100, 25 * width / 100, 10 * height / 100 );
-		mapEditor.setVisible( true );
-		add( mapEditor );
+		yComponent += yIncrement;
 
-		options = new JLabel( "Options" );
-		options.setBounds( 5, 75 * height / 100, 25 * width / 100, 10 * height / 100 );
-		options.setVisible( true );
-		add( options );
+		createServer = loadJLabel("CreateServer.png");
+		createServer.setBounds(5, yComponent, 25 * width / 100, yIncrement);
+		createServer.setVisible(true);
+		add(createServer);
 
-		quit = new JLabel( "Quit" );
-		quit.setBounds( 5, 90 * height / 100, 25 * width / 100, 10 * height / 100 );
-		quit.setVisible( true );
-		add( quit );
+		yComponent += yIncrement;
 
-		setVisible( true );
+		mapEditor = loadJLabel("MapEditor.png");
+		mapEditor.setBounds(5, yComponent, 25 * width / 100, yIncrement);
+		mapEditor.setVisible(true);
+		add(mapEditor);
+
+		yComponent += yIncrement;
+
+		options = loadJLabel("Options.png");
+		options.setBounds(5, yComponent, 25 * width / 100, yIncrement);
+		options.setVisible(true);
+		add(options);
+
+		yComponent += yIncrement;
+
+		quit = loadJLabel("Quit.png");
+		quit.setBounds(5, yComponent, 25 * width / 100, yIncrement);
+		quit.setVisible(true);
+		add(quit);
+
+		setVisible(true);
+	}
+
+	private JLabel loadJLabel(String fileName)
+	{
+
+		Image imageText = null;
+		try
+		{
+			imageText = ImageIO.read(new File(textImagePath + "/" + fileName));
+			// imageText = imageText.getScaledInstance(imageText.getWidth(this) * percentuage / 100,
+			// imageText.getHeight(this) * percentuage / 100,
+			// Image.SCALE_FAST);
+		} catch (IOException e)
+		{
+			// TODO Blocco catch generato automaticamente
+			e.printStackTrace();
+		}
+		return new JLabel(new ImageIcon(imageText));
 	}
 
 	@Override
-	protected void paintComponent( Graphics g )
+	protected void paintComponent(Graphics g)
 	{
-		super.paintComponent( g );
-		g.drawImage( backgroundImage, 0, 0, getWidth(), getHeight(), this );
+		super.paintComponent(g);
+		g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
 	}
 
-	public static void main( String[] args )
+	public static void main(String[] args)
 	{
 		int screenWidth = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
 		int screenHeight = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
 
 		JFrame frame = new JFrame();
-		frame.setLocation( 0, 0 );
+		frame.setLocation(0, 0);
 
-		frame.setSize( screenWidth, screenHeight );
+		frame.setSize(screenWidth, screenHeight);
 
-		MainMenuPanel mp = new MainMenuPanel( 0, 0, screenWidth, screenHeight );
-		frame.add( mp );
-		frame.setVisible( true );
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		MainMenuPanel mp = new MainMenuPanel(0, 0, screenWidth, screenHeight);
+		frame.add(mp);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
