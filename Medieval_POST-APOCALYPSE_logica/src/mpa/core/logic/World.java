025fcd4f2a6637f5ed83ae39cd498c0d677d0e8e
AruQ <arcuri.paola.93@gmail.com>
2015-09-14 19:03:26 +0200

    added player dimension changed world's method checkForCollisionInTheRange

--- Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/World.java
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/core/logic/World.java
@@ -1,5 +1,7 @@
 package mpa.core.logic;
 
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -23,14 +25,14 @@ public class World
 	private ArrayList<AbstractResourceProducer> resourceProducers = new ArrayList<AbstractResourceProducer>();
 	private ArrayList<Headquarter> headquarters = new ArrayList<>();
 
-	public World( float width, float height )
+	public World(float width, float height)
 	{
 		super();
 		this.width = width;
 		this.height = height;
 	}
 
-	public boolean addObject( AbstractPrivateProperty obj )
+	public boolean addObject(AbstractPrivateProperty obj)
 	{
 		float x = obj.getX();
 		float y = obj.getY();
@@ -40,21 +42,21 @@ public class World
 		float xMax = x + width / 2;
 		float yMin = y - height / 2;
 		float yMax = y + height / 2;
-		Vector2f xPair = new Vector2f( xMin, xMax );
-		Vector2f yPair = new Vector2f( yMin, yMax );
-		if( !objectX.containsKey( xPair ) )
-			objectX.put( xPair, new ArrayList<AbstractObject>() );
-		objectX.get( xPair ).add( obj );
-		if( !objectY.containsKey( yPair ) )
-			objectY.put( yPair, new ArrayList<AbstractObject>() );
-		objectY.get( yPair ).add( obj );
+		Vector2f xPair = new Vector2f(xMin, xMax);
+		Vector2f yPair = new Vector2f(yMin, yMax);
+		if (!objectX.containsKey(xPair))
+			objectX.put(xPair, new ArrayList<AbstractObject>());
+		objectX.get(xPair).add(obj);
+		if (!objectY.containsKey(yPair))
+			objectY.put(yPair, new ArrayList<AbstractObject>());
+		objectY.get(yPair).add(obj);
 
-		allObjects.add( obj );
+		allObjects.add(obj);
 
-		if( obj instanceof AbstractResourceProducer )
-			resourceProducers.add( ( AbstractResourceProducer ) obj );
-		if( obj instanceof Headquarter )
-			headquarters.add( ( Headquarter ) obj );
+		if (obj instanceof AbstractResourceProducer)
+			resourceProducers.add((AbstractResourceProducer) obj);
+		if (obj instanceof Headquarter)
+			headquarters.add((Headquarter) obj);
 		return true;
 	}
 
@@ -83,36 +85,35 @@ public class World
 		return allObjects;
 	}
 
-	public ArrayList<AbstractObject> getObjectsXInTheRange( float x )
+	public ArrayList<AbstractObject> getObjectsXInTheRange(float x)
 	{
 		Set<Vector2f> keySet = objectX.keySet();
 		ArrayList<AbstractObject> abstractObjectsX = new ArrayList<>();
-		for( Vector2f pos : keySet )
+		for (Vector2f pos : keySet)
 		{
-			if( x >= pos.x && x <= pos.y )
+			if (x >= pos.x && x <= pos.y)
 			{
-				abstractObjectsX.addAll( objectX.get( pos ) );
+				abstractObjectsX.addAll(objectX.get(pos));
 			}
 		}
 		return abstractObjectsX;
 	}
 
-	public ArrayList<AbstractObject> getObjectsInTheRange( float xMin, float xMax, float yMin,
-			float yMax )
+	public ArrayList<AbstractObject> getObjectsInTheRange(float xMin, float xMax, float yMin, float yMax)
 	{
 		ArrayList<AbstractObject> objects = new ArrayList<>();
-		ArrayList<AbstractObject> objectsX = getObjectsXInTheRange( ( int ) xMin, ( int ) xMax );
-		ArrayList<AbstractObject> objectsY = getObjectsYInTheRange( ( int ) yMin, ( int ) yMax );
+		ArrayList<AbstractObject> objectsX = getObjectsXInTheRange((int) xMin, (int) xMax);
+		ArrayList<AbstractObject> objectsY = getObjectsYInTheRange((int) yMin, (int) yMax);
 
-		for( AbstractObject objX : objectsX )
+		for (AbstractObject objX : objectsX)
 		{
 			Iterator<AbstractObject> it = objectsY.iterator();
-			while( it.hasNext() )
+			while (it.hasNext())
 			{
 				AbstractObject objectY = it.next();
-				if( objX == objectY )
+				if (objX == objectY)
 				{
-					objects.add( objX );
+					objects.add(objX);
 					it.remove();
 				}
 			}
@@ -126,113 +127,103 @@ public class World
 		return headquarters;
 	}
 
-	public ArrayList<AbstractObject> checkForCollision( float x, float y, float collisionRay )
+	public ArrayList<AbstractObject> checkForCollision(float x, float y, float collisionRay)
 	{
-		ArrayList<AbstractObject> objectsXInTheRange = getObjectsXInTheRange( x );
-		ArrayList<AbstractObject> objectsYInTheRange = getObjectsYInTheRange( y );
+		ArrayList<AbstractObject> objectsXInTheRange = getObjectsXInTheRange(x);
+		ArrayList<AbstractObject> objectsYInTheRange = getObjectsYInTheRange(y);
 
 		ArrayList<AbstractObject> collisions = new ArrayList<>();
 		ArrayList<AbstractObject> intersection = new ArrayList<>();
 
-		for( AbstractObject objX : objectsXInTheRange )
+		for (AbstractObject objX : objectsXInTheRange)
 		{
 			Iterator<AbstractObject> it = objectsYInTheRange.iterator();
 
-			while( it.hasNext() )
+			while (it.hasNext())
 			{
 				AbstractObject objY = it.next();
-				if( objX == objY )
+				if (objX == objY)
 				{
-					intersection.add( objX );
+					intersection.add(objX);
 					it.remove();
 				}
 			}
 		}
 
-		for( AbstractObject obj : intersection )
-			if( MyMath.distanceFloat( x, y, obj.getX(), obj.getY() ) - obj.getCollisionRay()
-					- collisionRay <= 0 )
-				collisions.add( obj );
+		for (AbstractObject obj : intersection)
+			if (MyMath.distanceFloat(x, y, obj.getX(), obj.getY()) - obj.getCollisionRay() - collisionRay <= 0)
+				collisions.add(obj);
 
 		return collisions;
 
 	}
 
-	public ArrayList<AbstractObject> checkForCollisionInTheRange( float xMin, float xMax,
-			float yMin, float yMax, float collisionRay )
+	public ArrayList<AbstractObject> checkForCollisionInTheRange(float xMin, float xMax, float yMin, float yMax, float collisionRay)
 	{
-		if( xMin > xMax )
-		{
-			float tmp = xMax;
-			xMax = xMin;
-			xMin = tmp;
-		}
-		if( yMin > yMax )
-		{
-			float tmp = yMax;
-			yMax = yMin;
-			yMin = tmp;
-		}
+
+		int extraRange = 10;
 		ArrayList<AbstractObject> collisions = new ArrayList<>();
-
-		ArrayList<AbstractObject> objectsInTheRange = getObjectsInTheRange( xMin, xMax, yMin, yMax );
-
-		// System.out.println( "la size di objectsInTheRange è " + objectsInTheRange.size() );
-
-		for( AbstractObject obj : objectsInTheRange )
+		Line2D.Float line = new Line2D.Float(xMin, yMin, xMax, yMax);
+		for (AbstractPrivateProperty abstractPrivateProperty : allObjects)
 		{
-			// System.out.println( "point to line :"
-			// + MyMath.pointToLineDistance( xMin, yMin, xMax, yMax, obj.getX(), obj.getY() ) );
-			// System.out.println( obj.getClass().getName() );
-			if( MyMath.pointToLineDistance( xMin, yMin, xMax, yMax, obj.getX(), obj.getY() )
-					- obj.getCollisionRay() - collisionRay <= 1f )
-				collisions.add( obj );
+			float xUpperLeftCollisionRay = abstractPrivateProperty.getX() - abstractPrivateProperty.getWidth() / 2
+					- abstractPrivateProperty.getWidth() * extraRange / 100;
+			float yUpperLeftCollisionRay = abstractPrivateProperty.getY() - abstractPrivateProperty.getHeight() / 2
+					- abstractPrivateProperty.getWidth() * extraRange / 100;
+
+			Rectangle2D.Float rect = new Rectangle2D.Float(xUpperLeftCollisionRay, yUpperLeftCollisionRay, abstractPrivateProperty.getWidth()
+					+ abstractPrivateProperty.getWidth() * extraRange / 100, abstractPrivateProperty.getHeight() + abstractPrivateProperty.getWidth()
+					* extraRange / 100);
+
+			if (line.intersects(rect))
+				collisions.add(abstractPrivateProperty);
+
 		}
-		System.out.println( collisions );
+		System.out.println("COLLISION " + collisions);
 
 		return collisions;
 
 	}
 
-	public ArrayList<AbstractObject> getObjectsYInTheRange( float y )
+	public ArrayList<AbstractObject> getObjectsYInTheRange(float y)
 	{
 		Set<Vector2f> keySet = objectY.keySet();
 		ArrayList<AbstractObject> abstractObjectsY = new ArrayList<>();
-		for( Vector2f pos : keySet )
+		for (Vector2f pos : keySet)
 		{
-			if( y >= pos.x && y <= pos.y )
+			if (y >= pos.x && y <= pos.y)
 			{
-				abstractObjectsY.addAll( objectY.get( pos ) );
+				abstractObjectsY.addAll(objectY.get(pos));
 			}
 		}
 		return abstractObjectsY;
 	}
 
-	public ArrayList<AbstractObject> getObjectsXInTheRange( float xMin, float xMax )
+	public ArrayList<AbstractObject> getObjectsXInTheRange(float xMin, float xMax)
 	{
 		Set<Vector2f> keySet = objectX.keySet();
 		ArrayList<AbstractObject> abstractObjectsX = new ArrayList<>();
-		for( Vector2f pos : keySet )
+		for (Vector2f pos : keySet)
 		{
-			if( ( pos.x <= xMax && pos.x >= xMin ) || ( pos.y <= xMax && pos.y >= xMin ) )
-				abstractObjectsX.addAll( objectX.get( pos ) );
-			else if( ( xMin >= pos.x && xMin <= pos.y ) || ( xMax >= pos.x && xMax <= pos.y ) )
-				abstractObjectsX.addAll( objectX.get( pos ) );
+			if ((pos.x <= xMax && pos.x >= xMin) || (pos.y <= xMax && pos.y >= xMin))
+				abstractObjectsX.addAll(objectX.get(pos));
+			else if ((xMin >= pos.x && xMin <= pos.y) || (xMax >= pos.x && xMax <= pos.y))
+				abstractObjectsX.addAll(objectX.get(pos));
 		}
 		// System.out.println( "numero abstract object x " + abstractObjectsX.size() );
 		return abstractObjectsX;
 	}
 
-	public ArrayList<AbstractObject> getObjectsYInTheRange( int yMin, int yMax )
+	public ArrayList<AbstractObject> getObjectsYInTheRange(int yMin, int yMax)
 	{
 		Set<Vector2f> keySet = objectY.keySet();
 		ArrayList<AbstractObject> abstractObjectsY = new ArrayList<>();
-		for( Vector2f pos : keySet )
+		for (Vector2f pos : keySet)
 		{
-			if( ( pos.x <= yMax && pos.x >= yMin ) || ( pos.y <= yMax && pos.y >= yMin ) )
-				abstractObjectsY.addAll( objectY.get( pos ) );
-			else if( ( yMin >= pos.x && yMin <= pos.y ) || ( yMax >= pos.x && yMax <= pos.y ) )
-				abstractObjectsY.addAll( objectY.get( pos ) );
+			if ((pos.x <= yMax && pos.x >= yMin) || (pos.y <= yMax && pos.y >= yMin))
+				abstractObjectsY.addAll(objectY.get(pos));
+			else if ((yMin >= pos.x && yMin <= pos.y) || (yMax >= pos.x && yMax <= pos.y))
+				abstractObjectsY.addAll(objectY.get(pos));
 		}
 		return abstractObjectsY;
 	}
@@ -240,12 +231,12 @@ public class World
 	@Override
 	public String toString()
 	{
-		String s = new String( "ObjectX \n" );
+		String s = new String("ObjectX \n");
 		Set<Vector2f> keySet = objectX.keySet();
-		for( Vector2f pos : keySet )
+		for (Vector2f pos : keySet)
 		{
 			s += pos.x + " " + pos.y + "\n";
-			for( AbstractObject obj : objectX.get( pos ) )
+			for (AbstractObject obj : objectX.get(pos))
 			{
 				s += obj.getClass() + " ";
 			}
@@ -253,10 +244,10 @@ public class World
 			s += "ObjectY \n";
 		}
 		keySet = objectY.keySet();
-		for( Vector2f pos : keySet )
+		for (Vector2f pos : keySet)
 		{
 			s += pos.x + " " + pos.y + "\n";
-			for( AbstractObject obj : objectY.get( pos ) )
+			for (AbstractObject obj : objectY.get(pos))
 			{
 				s += obj.getClass() + " ";
 			}
@@ -265,16 +256,16 @@ public class World
 		return s;
 	}
 
-	public AbstractObject pickedObject( float xGoal, float yGoal )
+	public AbstractObject pickedObject(float xGoal, float yGoal)
 	{
-		ArrayList<AbstractObject> objectsXInTheRange = getObjectsXInTheRange( xGoal );
-		ArrayList<AbstractObject> objectsYInTheRange = getObjectsYInTheRange( yGoal );
+		ArrayList<AbstractObject> objectsXInTheRange = getObjectsXInTheRange(xGoal);
+		ArrayList<AbstractObject> objectsYInTheRange = getObjectsYInTheRange(yGoal);
 
-		objectsXInTheRange.addAll( objectsYInTheRange );
-		for( AbstractObject abstractObject : objectsXInTheRange )
+		objectsXInTheRange.addAll(objectsYInTheRange);
+		for (AbstractObject abstractObject : objectsXInTheRange)
 		{
-			if( MyMath.distanceFloat( ( int ) abstractObject.getX(), ( int ) abstractObject.getY(),
-					( int ) xGoal, ( int ) yGoal ) - abstractObject.getCollisionRay() <= 0 )
+			if (MyMath.distanceFloat((int) abstractObject.getX(), (int) abstractObject.getY(), (int) xGoal, (int) yGoal)
+					- abstractObject.getCollisionRay() <= 0)
 			{
 				return abstractObject;
 			}
@@ -288,25 +279,25 @@ public class World
 		return resourceProducers;
 	}
 
-	public boolean addTower( Tower tower )
+	public boolean addTower(Tower tower)
 	{
 
-		if( !checkForCollision( tower.getX(), tower.getY(), tower.getCollisionRay() ).isEmpty() )
+		if (!checkForCollision(tower.getX(), tower.getY(), tower.getCollisionRay()).isEmpty())
 			return false;
 
-		addObject( tower );
+		addObject(tower);
 		return true;
 	}
 
-	public void destroyObject( AbstractObject obj )
+	public void destroyObject(AbstractObject obj)
 	{
-		for( Vector2f p : objectX.keySet() )
-			if( objectX.get( p ).contains( obj ) )
-				objectX.get( p ).remove( obj );
+		for (Vector2f p : objectX.keySet())
+			if (objectX.get(p).contains(obj))
+				objectX.get(p).remove(obj);
 
-		for( Vector2f p : objectY.keySet() )
-			if( objectY.get( p ).contains( obj ) )
-				objectY.get( p ).remove( obj );
+		for (Vector2f p : objectY.keySet())
+			if (objectY.get(p).contains(obj))
+				objectY.get(p).remove(obj);
 
 	}
 }
