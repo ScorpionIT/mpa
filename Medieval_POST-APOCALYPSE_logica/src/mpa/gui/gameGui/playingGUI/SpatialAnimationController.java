c7404dd6ac8381c90e9f7789648540f1b9b02322
AruQ <arcuri.paola.93@gmail.com>
2015-09-15 21:58:47 +0200

    added class SpatialAnimationController that handle walk, attack and death animation

---
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/gui/gameGui/playingGUI/SpatialAnimationController.java
@@ -0,0 +1,96 @@
+package mpa.gui.gameGui.playingGUI;
+
+import java.util.Random;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.AnimEventListener;
+import com.jme3.animation.LoopMode;
+import com.jme3.scene.Spatial;
+
+public class SpatialAnimationController implements AnimEventListener
+{
+	Spatial model;
+	private AnimChannel channel;
+	private AnimControl control;
+	private String playerName;
+	private GuiObjectManager guiObjectManager;
+
+	public SpatialAnimationController(Spatial model, GuiObjectManager guiObjectManager, String playerName)
+	{
+		this.model = model;
+		this.guiObjectManager = guiObjectManager;
+		this.playerName = playerName;
+		control = model.getControl(AnimControl.class);
+		control.addListener(this);
+		channel = control.createChannel();
+	}
+
+	@Override
+	public void onAnimChange(AnimControl arg0, AnimChannel arg1, String animName)
+	{
+		// TODO Stub di metodo generato automaticamente
+
+	}
+
+	@Override
+	public void onAnimCycleDone(AnimControl arg0, AnimChannel arg1, String animName)
+	{
+		if (animName.equals("death"))
+		{
+			guiObjectManager.removePlayer(playerName);
+		}
+	}
+
+	public void startWalkAnimation(float animationSpeed)
+	{
+		if (channel.getAnimationName() == null)
+		{
+			channel.setAnim("walk", animationSpeed);
+			channel.setLoopMode(LoopMode.Loop);
+		}
+	}
+
+	public void stopWalkAnimation()
+	{
+		if (channel.getAnimationName() != null && channel.getAnimationName().equals("walk"))
+		{
+			channel.reset(true);
+		}
+	}
+
+	public void startAttackAnimation(float animationSpeed)
+	{
+		if (control.getAnim("attack1") != null)
+		{
+			int rand = new Random().nextInt(2);
+			if (rand == 0)
+				channel.setAnim("attack1", animationSpeed);
+			else
+				channel.setAnim("attack", animationSpeed);
+
+		}
+		channel.setLoopMode(LoopMode.DontLoop);
+	}
+
+	public String getAnimationRunning()
+	{
+		if (channel.getTime() < channel.getAnimMaxTime())
+			return channel.getAnimationName();
+		else
+			return null;
+	}
+
+	public boolean startDeathAnimation()
+	{
+		if (control.getAnim("death") != null)
+		{
+			channel.setAnim("death");
+			channel.setLoopMode(LoopMode.DontLoop);
+			return true;
+		}
+		else
+			return false;
+
+	}
+}
