1483978ea0f494e81cfd2bf9b93b5fcfaa43bbae
Suinos <neongodtheymade@gmail.com>
2015-09-22 11:40:26 +0200

    Synch commit

--- Medieval_POST-APOCALYPSE_logica/src/mpa/gui/menuMap/MapPreview.java
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/gui/menuMap/MapPreview.java
@@ -29,21 +29,21 @@ public class MapPreview extends MpaPanel
 	private Map<JLabel, Pair<Float, Float>> headQuartersLabel = new HashMap<>();
 	private JPanel menuSinglePlayerPanel;
 	private Image backgroundImage;
-	private String texturePath = GameProperties.getInstance().getPath("TexturePath");
-	private String imagesPreviewPath = GameProperties.getInstance().getPath("ImagesPreviewPath");
+	private String texturePath = GameProperties.getInstance().getPath( "TexturePath" );
+	private String imagesPreviewPath = GameProperties.getInstance().getPath( "ImagesPreviewPath" );
 
-	public MapPreview(MenuSinglePlayerPanel menuSinglePlayerPanel)
+	public MapPreview( JPanel menuSinglePlayerPanel )
 	{
 		this.menuSinglePlayerPanel = menuSinglePlayerPanel;
-		this.setLayout(null);
+		this.setLayout( null );
 		try
 		{
-			backgroundImage = ImageIO.read(new File(texturePath + "/grass-pattern.png"));
-		} catch (IOException e1)
+			backgroundImage = ImageIO.read( new File( texturePath + "/grass-pattern.png" ) );
+		} catch( IOException e1 )
 		{
 			e1.printStackTrace();
 		}
-		this.setVisible(true);
+		this.setVisible( true );
 
 	}
 
@@ -52,101 +52,116 @@ public class MapPreview extends MpaPanel
 
 		try
 		{
-			File folder = new File(imagesPreviewPath);
+			File folder = new File( imagesPreviewPath );
 			File[] listOfFiles = folder.listFiles();
-			for (int i = 0; i < listOfFiles.length; i++)
+			for( int i = 0; i < listOfFiles.length; i++ )
 			{
-				if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".png"))
+				if( listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith( ".png" ) )
 				{
-					Image img = ImageIO.read(new File(imagesPreviewPath + "/" + listOfFiles[i].getName()));
-					images.put(listOfFiles[i].getName().substring(0, listOfFiles[i].getName().length() - 4), img);
+					Image img = ImageIO.read( new File( imagesPreviewPath + "/"
+							+ listOfFiles[i].getName() ) );
+					images.put(
+							listOfFiles[i].getName().substring( 0,
+									listOfFiles[i].getName().length() - 4 ), img );
 				}
 			}
 
-		} catch (IOException e)
+		} catch( IOException e )
 		{
 			e.printStackTrace();
 		}
 
 	}
 
-	public void loadMap(MapInfo mapInfo)
+	public void loadMap( MapInfo mapInfo )
 	{
 		this.mapInfo = mapInfo;
 
 		this.headQuartersLabel.clear();
 
-		for (Pair<Float, Float> headQuarterPosition : mapInfo.getHeadQuarters())
+		for( Pair<Float, Float> headQuarterPosition : mapInfo.getHeadQuarters() )
 		{
-			Image image = images.get("headQuarter");
-			image = image.getScaledInstance(W(GameProperties.getInstance().getObjectWidth("headQuarter")), H(GameProperties.getInstance()
-					.getObjectHeight("headQuarter")), 0);
+			Image image = images.get( "headQuarter" );
+			image = image.getScaledInstance(
+					W( GameProperties.getInstance().getObjectWidth( "headQuarter" ) ),
+					H( GameProperties.getInstance().getObjectHeight( "headQuarter" ) ), 0 );
 
-			ImageIcon imageIcon = new ImageIcon(image);
-			JLabel jLabel = new JLabel(imageIcon);
-			jLabel.addMouseListener(new MouseAdapter()
+			ImageIcon imageIcon = new ImageIcon( image );
+			JLabel jLabel = new JLabel( imageIcon );
+			jLabel.addMouseListener( new MouseAdapter()
 			{
 				@Override
-				public void mouseReleased(MouseEvent e)
+				public void mouseReleased( MouseEvent e )
 				{
 					Set<JLabel> keys = MapPreview.this.headQuartersLabel.keySet();
-					for (JLabel l : keys)
-						if (MapPreview.this.headQuartersLabel.get(l).equals(
-								((MenuSinglePlayerPanel) MapPreview.this.menuSinglePlayerPanel).getSelectedHQ()))
+					for( JLabel l : keys )
+						if( MapPreview.this.headQuartersLabel.get( l ).equals(
+								( ( MenuSinglePlayerPanel ) MapPreview.this.menuSinglePlayerPanel )
+										.getSelectedHQ() ) )
 						{
-							l.setBorder(BorderFactory.createEmptyBorder());
+							l.setBorder( BorderFactory.createEmptyBorder() );
 						}
-					((JLabel) e.getSource()).setBorder(BorderFactory.createLineBorder(Color.red));
-					((MenuSinglePlayerPanel) MapPreview.this.menuSinglePlayerPanel).setSelectedHeadQuarter(MapPreview.this.headQuartersLabel.get(e
-							.getSource()));
+					( ( JLabel ) e.getSource() ).setBorder( BorderFactory
+							.createLineBorder( Color.red ) );
+					( ( MenuSinglePlayerPanel ) MapPreview.this.menuSinglePlayerPanel )
+							.setSelectedHeadQuarter( MapPreview.this.headQuartersLabel.get( e
+									.getSource() ) );
 
 				}
 
-			});
-			jLabel.setBounds(graphicX(headQuarterPosition.getFirst()), graphicY(headQuarterPosition.getSecond()), W(GameProperties.getInstance()
-					.getObjectWidth("headQuarter")), H(GameProperties.getInstance().getObjectHeight("headQuarter")));
-			this.add(jLabel);
+			} );
+			jLabel.setBounds( graphicX( headQuarterPosition.getFirst() ),
+					graphicY( headQuarterPosition.getSecond() ), W( GameProperties.getInstance()
+							.getObjectWidth( "headQuarter" ) ), H( GameProperties.getInstance()
+							.getObjectHeight( "headQuarter" ) ) );
+			this.add( jLabel );
 
-			headQuartersLabel.put(jLabel, headQuarterPosition);
+			headQuartersLabel.put( jLabel, headQuarterPosition );
 		}
 	}
 
 	@Override
-	protected void paintComponent(Graphics g)
+	protected void paintComponent( Graphics g )
 	{
 
-		super.paintComponent(g);
-		setBackgroundPatternImage(g);
+		super.paintComponent( g );
+		setBackgroundPatternImage( g );
 
-		if (mapInfo == null)
+		if( mapInfo == null )
 		{
 			return;
 		}
 
-		for (Pair<Float, Float> woodPosition : mapInfo.getWoods())
+		for( Pair<Float, Float> woodPosition : mapInfo.getWoods() )
 		{
 
 			// System.out.println(woodPosition.getFirst() + " " + woodPosition.getSecond());
-			g.drawImage(images.get("wood"), graphicX(woodPosition.getFirst()), graphicY(woodPosition.getSecond()), W(GameProperties.getInstance()
-					.getObjectWidth("wood")), H(GameProperties.getInstance().getObjectHeight("wood")), this);
+			g.drawImage( images.get( "wood" ), graphicX( woodPosition.getFirst() ),
+					graphicY( woodPosition.getSecond() ), W( GameProperties.getInstance()
+							.getObjectWidth( "wood" ) ), H( GameProperties.getInstance()
+							.getObjectHeight( "wood" ) ), this );
 			// g.drawImage(images.get("wood"), graphicX(woodPosition.getFirst()),
 			// graphicY(woodPosition.getSecond()), images.get("wood").getWidth(null),
 			// images.get("wood").getHeight(null), this);
 		}
-		for (Pair<Float, Float> fieldPosition : mapInfo.getFields())
+		for( Pair<Float, Float> fieldPosition : mapInfo.getFields() )
 		{
 
-			g.drawImage(images.get("field"), graphicX(fieldPosition.getFirst()), graphicY(fieldPosition.getSecond()), W(GameProperties.getInstance()
-					.getObjectWidth("field")), H(GameProperties.getInstance().getObjectHeight("field")), this);
+			g.drawImage( images.get( "field" ), graphicX( fieldPosition.getFirst() ),
+					graphicY( fieldPosition.getSecond() ), W( GameProperties.getInstance()
+							.getObjectWidth( "field" ) ), H( GameProperties.getInstance()
+							.getObjectHeight( "field" ) ), this );
 			// g.drawImage(images.get("field"), graphicX(fieldPosition.getFirst()),
 			// graphicY(fieldPosition.getSecond()),
 			// images.get("field").getWidth(null), images.get("field").getHeight(null), this);
 		}
 
-		for (Pair<Float, Float> cavesPosition : mapInfo.getCaves())
+		for( Pair<Float, Float> cavesPosition : mapInfo.getCaves() )
 		{
-			g.drawImage(images.get("cave"), graphicX(cavesPosition.getFirst()), graphicY(cavesPosition.getSecond()), W(GameProperties.getInstance()
-					.getObjectWidth("cave")), H(GameProperties.getInstance().getObjectHeight("cave")), this);
+			g.drawImage( images.get( "cave" ), graphicX( cavesPosition.getFirst() ),
+					graphicY( cavesPosition.getSecond() ), W( GameProperties.getInstance()
+							.getObjectWidth( "cave" ) ), H( GameProperties.getInstance()
+							.getObjectHeight( "cave" ) ), this );
 			// g.drawImage(images.get("cave"), graphicX(cavesPosition.getFirst()),
 			// graphicY(cavesPosition.getSecond()), images.get("cave")
 			// .getWidth(null), images.get("cave").getHeight(null), this);
@@ -158,28 +173,29 @@ public class MapPreview extends MpaPanel
 
 	}
 
-	private void setBackgroundPatternImage(Graphics g)
+	private void setBackgroundPatternImage( Graphics g )
 	{
-		int backgroundWidth = backgroundImage.getWidth(this);
-		int backgroundHeight = backgroundImage.getWidth(this);
+		int backgroundWidth = backgroundImage.getWidth( this );
+		int backgroundHeight = backgroundImage.getWidth( this );
 		int maxI = this.getWidth() / backgroundWidth;
 		int maxJ = this.getHeight() / backgroundHeight;
 
-		for (int i = 0; i <= maxI; i++)
+		for( int i = 0; i <= maxI; i++ )
 		{
-			for (int j = 0; j <= maxJ; j++)
+			for( int j = 0; j <= maxJ; j++ )
 			{
-				g.drawImage(backgroundImage, i * backgroundWidth, j * backgroundHeight, backgroundWidth, backgroundHeight, this);
+				g.drawImage( backgroundImage, i * backgroundWidth, j * backgroundHeight,
+						backgroundWidth, backgroundHeight, this );
 			}
 		}
 
 	}
 
 	@Override
-	public void setMapDimension(float width, float height)
+	public void setMapDimension( float width, float height )
 	{
 
-		super.setMapDimension(width, height);
+		super.setMapDimension( width, height );
 		loadImages();
 	}
 
---
+++ Medieval_POST-APOCALYPSE_logica/src/mpa/gui/multiPlayerMenu/MultiplayerMenuPanel.java
@@ -0,0 +1,264 @@
+package mpa.gui.multiPlayerMenu;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Panel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+
+import mpa.core.ai.DifficultyLevel;
+import mpa.core.logic.MapInfo;
+import mpa.core.logic.Pair;
+import mpa.core.logic.WorldLoader;
+import mpa.core.util.GameProperties;
+import mpa.core.util.MapFromXMLCreator;
+import mpa.gui.menuMap.DifficultyPanel;
+import mpa.gui.menuMap.InputNamePanel;
+import mpa.gui.menuMap.MapListPanel;
+import mpa.gui.menuMap.MapPreview;
+
+import org.jdom2.JDOMException;
+
+import com.jme3.system.AppSettings;
+
+public class MultiplayerMenuPanel extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	private MapInfo mapInfo;
+	private MapListPanel mapListPanel;
+	private MapPreview mapPreview;
+	private InputNamePanel inputNamePanel;
+	private DifficultyPanel difficultyPanel;
+	private WorldLoader worldLoader = new WorldLoader();
+	private Pair<Float, Float> selectedHQ = null;
+	private String playerName = null;
+	private JLabel buttonPlay;
+	private Image backgroundImage;
+	private int selectedHQIndex = 0;
+	int screenWidth;
+	int screenHeight;
+	DifficultyLevel difficultyLevelSelected = null;
+	private String textImagePath = GameProperties.getInstance().getPath( "TextImagePath" );
+
+	public MultiplayerMenuPanel( int x, int y, int width, int height )
+	{
+
+		this.setSize( width, height );
+		this.setLocation( x, y );
+		this.setLayout( null );
+		screenWidth = this.getWidth();
+		screenHeight = this.getHeight();
+
+		addMapPreviewPanel();
+
+		addInputNamePanel();
+
+		Image imagePlay = null;
+		try
+		{
+			imagePlay = ImageIO.read( new File( textImagePath + "/Play.png" ) );
+			imagePlay = imagePlay.getScaledInstance( screenWidth * 20 / this.getWidth() * 10, 40,
+					Image.SCALE_FAST );
+			buttonPlay = new JLabel( new ImageIcon( imagePlay ) );
+		} catch( IOException e2 )
+		{
+			// TODO Blocco catch generato automaticamente
+			e2.printStackTrace();
+		}
+		buttonPlay.setBounds( this.getWidth() - imagePlay.getWidth( this ),
+				52 + screenHeight * 80 / 100, imagePlay.getWidth( this ),
+				imagePlay.getHeight( this ) );
+		buttonPlay.addMouseListener( new MouseAdapter()
+		{
+			@Override
+			public void mouseReleased( MouseEvent e )
+			{
+				playerName = inputNamePanel.getPlayerName();
+				if( playerName == null || playerName.equals( "" ) )
+				{
+					setButtonsStyle();
+					JOptionPane.showMessageDialog( new Frame(), "Inserisci il nome che preferisci",
+							"Message", JOptionPane.PLAIN_MESSAGE );
+				}
+				else if( selectedHQ == null )
+					JOptionPane.showMessageDialog( new Frame(), "Seleziona il quartier generale",
+							"Message", JOptionPane.PLAIN_MESSAGE );
+				else if( difficultyLevelSelected == null )
+					JOptionPane.showMessageDialog( new Frame(), "Seleziona la difficoltà",
+							"Message", JOptionPane.PLAIN_MESSAGE );
+				else
+				{
+
+					Thread t = new Thread( new Runnable()
+					{
+						@Override
+						public void run()
+						{
+							mpa.gui.gameGui.playingGUI.GameGui app = new mpa.gui.gameGui.playingGUI.GameGui(
+									playerName );
+							AppSettings gameSettings = new AppSettings( false );
+							// gameSettings.setResolution( 800, 600 );
+							// GraphicsDevice device = GraphicsEnvironment
+							// .getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+							// gameSettings.setResolution( screenWidth, screenHeight );
+							// gameSettings.setFullscreen( true );
+
+							gameSettings.setResolution( java.awt.Toolkit.getDefaultToolkit()
+									.getScreenSize().width, java.awt.Toolkit.getDefaultToolkit()
+									.getScreenSize().height );
+							gameSettings.setFullscreen( true );
+							gameSettings.setVSync( false );
+							gameSettings.setTitle( "Stellar Conquest" );
+							gameSettings.setUseInput( true );
+							gameSettings.setFrameRate( 500 );
+							gameSettings.setSamples( 0 );
+							gameSettings.setRenderer( "LWJGL-OpenGL2" );
+							app.setSettings( gameSettings );
+							app.setShowSettings( false );
+
+							// disable statistics
+							app.setDisplayFps( false );
+							app.setDisplayStatView( false );
+							app.start();
+						}
+					} );
+					t.start();
+				}
+			}
+
+		} );
+
+		this.add( buttonPlay );
+		try
+		{
+			backgroundImage = ImageIO.read( new File( "Assets/BackgroundImages/mainMenu.jpg" ) );
+		} catch( IOException e1 )
+		{
+			e1.printStackTrace();
+		}
+		this.setVisible( true );
+	}
+
+	private void setButtonsStyle()
+	{
+
+		Font font = null;
+		try
+		{
+			font = Font
+					.createFont( Font.PLAIN, new FileInputStream( "./Assets/Fonts/KELMSCOT.TTF" ) );
+		} catch( FontFormatException | IOException e )
+		{
+			// TODO Blocco catch generato automaticamente
+			e.printStackTrace();
+		}
+		font = font.deriveFont( Font.PLAIN, 15 );
+		UIManager.put( "Label.font", font );
+		UIManager.put( "Button.font", new Font( "URW Chancery L", Font.BOLD, 13 ) );
+	}
+
+	public void setMap( String mapName )
+	{
+		mapPreview.removeAll();
+		try
+		{
+			worldLoader.loadMapInfo( new MapFromXMLCreator(), mapName );
+		} catch( JDOMException | IOException e )
+		{
+			JOptionPane.showMessageDialog( this,
+					"Sorry but this map seems to be lost or damaged :(", "MAP ERROR",
+					JOptionPane.ERROR_MESSAGE );
+
+			// TODO rimuovere la mappa danneggiata (o persa) dalla lista!!!!!
+		}
+		mapInfo = worldLoader.getMapInfo();
+		mapPreview.setMapDimension( mapInfo.getWidth(), mapInfo.getHeight() );
+		mapPreview.loadMap( mapInfo );
+		selectedHQ = null;
+		this.repaint();
+	}
+
+	public void setSelectedHeadQuarter( Pair<Float, Float> headQuarterPosition )
+	{
+		if( mapInfo.getHeadQuarters().contains( headQuarterPosition ) )
+			selectedHQ = headQuarterPosition;
+	}
+
+	public Pair<Float, Float> getSelectedHQ()
+	{
+		return selectedHQ;
+	}
+
+	public void setPlayerName( String playerName )
+	{
+		this.playerName = playerName;
+	}
+
+	public void setDifficultyLevel( DifficultyLevel difficultyLevel )
+	{
+		this.difficultyLevelSelected = difficultyLevel;
+	}
+
+	private void addMapPreviewPanel()
+	{
+		mapPreview = new MapPreview( this );
+		mapPreview.setBounds( screenWidth * 55 / 100, 30, screenWidth * 45 / 100 - 30,
+				screenHeight * 55 / 100 );
+		mapPreview.repaint();
+		this.add( mapPreview );
+	}
+
+	private void addInputNamePanel()
+	{
+		inputNamePanel = new InputNamePanel();
+		inputNamePanel.setBounds( 40, 45, screenWidth * 50 / 100, screenHeight * 40 / 100 );
+		inputNamePanel.setOpaque( false );
+		this.add( inputNamePanel );
+	}
+
+	@Override
+	protected void paintComponent( Graphics g )
+	{
+		super.paintComponent( g );
+		g.drawImage( backgroundImage, 0, 0, screenWidth, screenHeight, this );
+	}
+
+	public static void main( String[] args )
+	{
+
+		int screenWidth = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+		int screenHeight = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
+
+		JFrame frame = new JFrame();
+		frame.setLocation( 0, 0 );
+
+		frame.setSize( screenWidth, screenHeight );
+		MultiplayerMenuPanel mainMenuGamePanel = new MultiplayerMenuPanel( screenWidth * 15 / 100,
+				screenHeight * 10 / 100, screenWidth * 70 / 100, screenHeight * 80 / 100 );
+
+		frame.setContentPane( new Panel() );
+		frame.getContentPane().setBackground( Color.BLACK );
+		frame.getContentPane().add( mainMenuGamePanel );
+		frame.getContentPane().setLayout( null );
+		frame.getContentPane().setVisible( true );;
+		frame.setVisible( true );
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	}
+
+}
